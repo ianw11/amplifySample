74a8b330f41c40eb90473e0c797d98c18c87c1ee
Ian Washburne <ianw11@gmail.com>
2019-11-29 17:06:13 -0800

    IT WORKS

--- App.js
+++ App.js
@@ -1,114 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {useEffect, useReducer} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import API, {graphqlOperation} from '@aws-amplify/api';
+import PubSub from '@aws-amplify/pubsub';
+import {createTodo} from './src/graphql/mutations';
+import {listTodos} from './src/graphql/queries';
+import {onCreateTodo} from './src/graphql/subscriptions';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import config from './aws-exports';
+API.configure(config); // Configure Amplify
+PubSub.configure(config);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+async function createNewTodo() {
+  const todo = {name: 'Use AppSync', description: 'Realtime and Offline'};
+  await API.graphql(graphqlOperation(createTodo, {input: todo}));
+}
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const initialState = {todos: []};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'QUERY':
+      return {...state, todos: action.todos};
+    case 'SUBSCRIPTION':
+      return {...state, todos: [...state.todos, action.todo]};
+    default:
+      return state;
+  }
 };
 
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    getData();
+
+    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+      next: eventData => {
+        console.log(eventData);
+        console.log(eventData.value);
+        const todo = eventData.value.data.onCreateTodo;
+        dispatch({type: 'SUBSCRIPTION', todo});
+      },
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
+  async function getData() {
+    const todoData = await API.graphql(graphqlOperation(listTodos))
+    dispatch({type: 'QUERY', todos: todoData.data.listTodos.items});
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={createNewTodo} title="Create Todo" />
+      {state.todos.map((todo, i) => (
+        <Text key={todo.id}>
+          {todo.name} : {todo.description}
+        </Text>
+      ))}
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    backgroundColor: '#ddeeff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
   },
 });
-
-export default App;
